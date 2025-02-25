3189b45efeecd2af840a12ebef10c088ba422c04
VeronikaMiteva <mitevver@students.zhaw.ch>
2025-02-25 21:22:33 +0100

    anfänglich Streamlit app

---
+++ README.md
@@ -0,0 +1,3 @@
+# BMLD-Inf2-AppV0
+
+Das leere Gerüst um eine App aufzubauen.
---
+++ Start.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Meine erste Streamlit App")
+
+st.write("Diese App wurde von Anne Müller und Hans Meier entwickelt.")
