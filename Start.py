c306800d3e4ef995235d80ecb25ecf56a54446ad
VeronikaMiteva <mitevver@students.zhaw.ch>
2025-02-25 22:03:41 +0100

    E-mail hinzugefügt

--- Start.py
+++ Start.py
@@ -3,4 +3,9 @@ import pandas as pd
 
 st.title("Meine erste Streamlit App")
 
-st.write("Diese App wurde von Veronika Miteva und Phiphi Chung entwickelt.")
+st.write("Diese App wurde von Veronika Miteva und Phiphi Cung entwickelt.")
+
+student_email_veroninka = "mitevver@students.zhaw.ch"
+student_email_phiphi = "cungphi1@students.zhaw.ch"
+st.write(f"{student_email_veroninka}")
+st.write(f"{student_email_phiphi}")
