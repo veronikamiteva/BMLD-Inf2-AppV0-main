a514c7c017a3a340315b9725b407b69415718f44
VeronikaMiteva <mitevver@students.zhaw.ch>
2025-02-26 23:15:52 +0100

    Text geändert

--- Start.py
+++ Start.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 st.title("Meine erste Streamlit App")
 
-st.write("Diese App wurde von Veronika Miteva und Phiphi Cung entwickelt.")
+st.write("Diese App wurde von den folgenden Personen entwickelt:")
 
 student_email_veroninka = "mitevver@students.zhaw.ch"
 student_email_phiphi = "cungphi1@students.zhaw.ch"
-st.write(f"{student_email_veroninka}")
-st.write(f"{student_email_phiphi}")
+st.write("Veronika Miteva - " + f"{student_email_veroninka}")
+st.write("Phi Phi Cung - " + f"{student_email_phiphi}")
+
+st.write("Diese App dient als leeres Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW).")
