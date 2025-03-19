c615274fb05b1e19451b8e1ca0af039532183120
VeronikaMiteva <mitevver@students.zhaw.ch>
2025-03-19 10:09:26 +0100

    added function

--- pages/1_Unterseite A.py
+++ pages/1_Unterseite A.py
@@ -3,10 +3,11 @@ import streamlit as st
 
 st.title('BMI Werte')
 
-data_df = st.session_state['data_df']
-if data_df.empty:
+if 'data_df' not in st.session_state or st.session_state['data_df'].empty:
     st.info('Keine BMI Daten vorhanden. Berechnen Sie Ihren BMI auf der Startseite.')
     st.stop()
+else:
+    data_df = st.session_state['data_df']
 
 # Ensure 'timestamp' column is in datetime format
 data_df['timestamp'] = pd.to_datetime(data_df['timestamp'], errors='coerce')
